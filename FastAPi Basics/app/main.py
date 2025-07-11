ccbec97a07a60b51836081b13e58384e0fd2411a
Alok <144205496+Alokiitdh@users.noreply.github.com>
2025-07-11 14:51:15 +0530

    Testing get enndpoint

--- FastAPi Basics/app/main.py
+++ FastAPi Basics/app/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Differnt type of endpoints
+# GET, POST, PUT, DELETE
+
+# GET: Retrieve data
+# POST: Create new data
+# PUT: Update existing data
+# DELETE: Remove data
+
+# Define a simple root endpoint 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the PDF Chatbot API!"}
+
